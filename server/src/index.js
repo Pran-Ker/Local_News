8697ad1cea28f2da0612c1eb2678ea8de5b39a83
Prannay Hebbar <hebbarpran@gmail.com>
2021-10-07 18:29:43 +0530

    Simplifying

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares')
 
 const app = express();
 app.use(morgan('common'));
@@ -16,21 +17,9 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error('Not Found - 4(req.orginalUrl');
-  res.status(404);
-  next(error);
-});
 
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  // Check whether status code is 200, then convert to 500, else res.status code
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '',
-  });
-}); 
+app.use(middlewares.errorHandler);
+app.use(middlewares.notFound);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
